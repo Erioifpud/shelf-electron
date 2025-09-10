09516d2c01eb2ecbba7e7118035c321e135e96ba
Erio <erioifpud.cn@gmail.com>
2025-09-11 04:02:56 +0800

    feat: 新增站点卡片选中样式

--- packages/plugin-example/renderer/pages/SourceList/index.tsx
+++ packages/plugin-example/renderer/pages/SourceList/index.tsx
@@ -2,11 +2,63 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Toaster } from "@/components/ui/sonner";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 import useRuleStore from "@/store/rule";
 import { Site } from "@/store/rule/type";
 import { EditIcon, PlusIcon, SearchIcon, ViewIcon } from "lucide-react";
 import { memo, useCallback, useMemo, useState } from "react";
+import { useParams } from "react-router";
 import { Outlet, useFetcher, useNavigate } from "react-router";
+
+interface SiteCardProps {
+  site: Site
+  onEditClick: (site: Site) => void
+  className?: HTMLDivElement['className']
+}
+
+const SiteCard = memo(({ site, className = '', onEditClick }: SiteCardProps) => {
+  return (
+    <Tooltip key={site.id}>
+      <TooltipTrigger asChild>
+        <div
+          className={cn(
+            'rounded-lg border border-gray-300 p-4 bg-gray-50 flex flex-col',
+            className,
+          )}
+        >
+          {/* 标题和状态 */}
+          <div className="flex flex-nowrap gap-2 items-center">
+            <div className="grow truncate text-sm">
+              {site.common.siteName}
+            </div>
+            <div className="bg-green-100 text-green-600 font-semibold text-xs flex items-center px-2 py-2 rounded-sm">
+              {site.common.version}
+            </div>
+          </div>
+          {/* 网址 */}
+          <div className="text-xs text-gray-400">
+            {site.common.siteUrl}
+          </div>
+          {/* 三栏状态，如果有 */}
+          {/* 工具栏 */}
+          <div className="flex gap-2 mt-4">
+            <Button size="sm" variant="default" className="grow">
+              <ViewIcon className="h-4 w-4" /> Preview
+            </Button>
+            <Button size="sm" variant="outline" className="grow" onClick={() => onEditClick(site)}>
+              <EditIcon className="h-4 w-4" /> Edit
+            </Button>
+          </div>
+        </div>
+      </TooltipTrigger>
+      {!!site.common.description.trim() && (
+        <TooltipContent>
+          <p>{site.common.description.trim()}</p>
+        </TooltipContent>
+      )}
+    </Tooltip> 
+  )
+})
 
 // 新建站点等同于创建一个空配置放入 store，再选中他的 id
 const Source = memo(() => {
@@ -14,6 +66,7 @@ const Source = memo(() => {
 
   const navigate = useNavigate()
   const fetcher = useFetcher()
+  const params = useParams()
 
   const toEditPage = useCallback((site: Site) => {
     navigate({
@@ -52,40 +105,15 @@ const Source = memo(() => {
         <div className="grow h-full flex flex-col overflow-auto gap-3 p-3">
           {sites.map(site => {
             return (
-              <Tooltip key={site.id}>
-                <TooltipTrigger asChild>
-                  <div className="rounded-lg border border-gray-300 p-4 bg-gray-50 flex flex-col">
-                    {/* 标题和状态 */}
-                    <div className="flex flex-nowrap gap-2 items-center">
-                      <div className="grow truncate text-sm">
-                        {site.common.siteName}
-                      </div>
-                      <div className="bg-green-100 text-green-600 font-semibold text-xs flex items-center px-2 py-2 rounded-sm">
-                        {site.common.version}
-                      </div>
-                    </div>
-                    {/* 网址 */}
-                    <div className="text-xs text-gray-400">
-                      {site.common.siteUrl}
-                    </div>
-                    {/* 三栏状态，如果有 */}
-                    {/* 工具栏 */}
-                    <div className="flex gap-2 mt-4">
-                      <Button size="sm" variant="default" className="grow">
-                        <ViewIcon className="h-4 w-4" /> Preview
-                      </Button>
-                      <Button size="sm" variant="outline" className="grow" onClick={() => toEditPage(site)}>
-                        <EditIcon className="h-4 w-4" /> Edit
-                      </Button>
-                    </div>
-                  </div>
-                </TooltipTrigger>
-                {!!site.common.description.trim() && (
-                  <TooltipContent>
-                    <p>{site.common.description.trim()}</p>
-                  </TooltipContent>
+              <SiteCard
+                key={site.id}
+                site={site}
+                onEditClick={toEditPage}
+                className={cn(
+                  'transition-shadow',
+                  params.sourceId === site.id && 'shadow shadow-primary/20'
                 )}
-              </Tooltip> 
+              />
             )
           })}
         </div>
