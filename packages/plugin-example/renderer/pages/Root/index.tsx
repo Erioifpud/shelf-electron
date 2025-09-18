3909aaf521bba96eb9d3ce45d6c6fb504d531943
Erio <erioifpud.cn@gmail.com>
2025-09-18 21:50:55 +0800

    feat: 调整应用名称 refactor: 移除无用代码

--- packages/plugin-example/renderer/pages/Root/index.tsx
+++ packages/plugin-example/renderer/pages/Root/index.tsx
@@ -1,30 +1,5 @@
 // src/routes/Root.jsx
-import React from 'react';
-import { Outlet, Link, useMatches, useNavigation, Route, NavLink } from 'react-router';
-
-// 面包屑组件
-function Breadcrumbs() {
-  const matches = useMatches();
-  // 过滤掉没有 crumb 函数的路由
-  const crumbs = matches
-    .filter((match) => Boolean(match.handle?.crumb))
-    .map((match) => {
-      // 调用 handle.crumb 函数，并传入 loader 数据
-      const crumb = match.handle.crumb(match.data);
-      return (
-        <li key={match.id} className="breadcrumb-item">
-          <Link to={match.pathname}>{crumb}</Link>
-        </li>
-      );
-    });
-
-  return (
-    <nav aria-label="breadcrumb">
-      <ol className="breadcrumb">{crumbs}</ol>
-    </nav>
-  );
-}
-
+import { Outlet } from 'react-router';
 
 export default function Root() {
   // const navigation = useNavigation();
@@ -32,7 +7,7 @@ export default function Root() {
   return (
     <div className="h-full flex flex-col">
       <header className="p-4 border-b border-gray-300 bg-gray-100 shrink-0 flex items-center select-none">
-        <h1 className="text-xl font-bold">My Awesome App</h1>
+        <h1 className="text-xl font-bold">Shelf</h1>
         {/* <Breadcrumbs /> 动态面包屑组件 */}
       </header>
       <main className="grow h-full overflow-hidden">
