ba13c86d4d58664505a13b0be03737f0a1999ac2
Erio <erioifpud.cn@gmail.com>
2025-09-29 19:28:43 +0800

    feat: 实现详情页雏形

--- packages/plugin-example/renderer/pages/BookDetail/loader.ts
+++ packages/plugin-example/renderer/pages/BookDetail/loader.ts
@@ -1,5 +1,20 @@
-export function bookDetailLoader({ params }) {
-  return {
-    bookDetail: {}
+import { getScrapingConfig } from "@/lib/loader"
+import { getService } from "@eleplug/elep/renderer"
+import { LoaderFunctionArgs } from "react-router"
+import { CrawlerApi } from "src/crawler/api"
+
+export async function bookDetailLoader({ params }: LoaderFunctionArgs) {
+  const { scrapingConfig, baseUrl } = getScrapingConfig(params, 'detailView')
+
+  const service = await getService<CrawlerApi>()
+  if (!scrapingConfig) {
+    return {
+      item: []
+    }
   }
+  const ret = await service.crawl.run.ask(scrapingConfig, {
+    baseUrl,
+  })
+
+  return ret
 }
